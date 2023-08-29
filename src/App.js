847715aaa1eb3ac7fd5cb77424c92a7e2168bacf
Jesse Ge <71725877+jesse1337@users.noreply.github.com>
2023-08-28 20:35:50 -0700

    multi users

--- src/App.js
+++ src/App.js
@@ -227,7 +227,6 @@ function App() {
           </div>
         </>
       )}
-
       <h2 className="today">
         today is
         <text className="date">
@@ -237,7 +236,6 @@ function App() {
         </text>
       </h2>
       <h1 className="todo"> to do ✗ :</h1>
-
       <input
         className="input"
         type="text"
@@ -248,7 +246,6 @@ function App() {
         }
         onKeyDown={handleKeyDown}
       />
-
       <button className="addButton" title="add" onClick={() => addItem()}>
         <text className="addText">✎</text>
       </button>
@@ -259,7 +256,6 @@ function App() {
       >
         <text className="refreshText">☄︎</text>
       </button>
-
       <ul className="ul">
         {state.items.map((item) => {
           return (
@@ -276,7 +272,7 @@ function App() {
           );
         })}
       </ul>
-
+      // testing git ddodododo
       <br />
       <text className="msg">{displayMsg()}</text>
       <h2 className="done">done ✔︎ :</h2>
